5880458ac77aee83e7fe0311f48f14799fbeee18
Николай <ShajtorNN@giapdc.ru>
2022-05-16 20:22:18 +0300

    UPD added editUserPage component & bug fixing

--- src/api/index.js
+++
@@ -1,9 +0,0 @@
-import users from './fake.api/user.api'
-import professions from './fake.api/professions.api'
-import qualities from './fake.api/qualities.api'
-const api = {
-  users,
-  professions,
-  qualities
-}
-export default api
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from '../src/App'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 import 'bootstrap/dist/css/bootstrap.css'
+import App from './app/App'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
